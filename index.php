e332185a6de2b0435418f1669258f2d01ace8e7e
OAsiTz <teven147q@gmail.com>
2022-10-26 12:06:19 +0700

    register_save

--- index.php
+++ index.php
@@ -7,84 +7,84 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Webboard KaKKak</title>
+    <title>Webboard OAsiTz</title>
     <!-- CSS only -->
-<!-- CSS only -->
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
-<!-- JavaScript Bundle with Popper -->
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
+    <!-- JavaScript Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
 </head>
 <?php
-    if(!isset($_SESSION['id'])){
-?>
-<body>
-    <div class="container-fluid">
-    <h1><center>Webboard OAsiTz</center></h1>
-    <?php include "nav.php";
+    if(!isset($_SESSION["id"])){
     ?>
-    <hr>
-    หมวดหมู่:
-    <select name = "category">
-        <option value="all">--ทั้งหมด--</option>
-        <option value="general">เรื่องทั่วไป</option>
-        <option value="study">เรื่องเรียน</option>
-    </select>
-    <a href="login.php"style="float:right">เข้าสู่ระบบ</a>
-    <br>
-    <a href="newpost.php">สร้างกระทู่ใหม่</a>
-    <br>
-    <ul> <form action="post.php" method="get">
+    <body>
+        <div class="container-fluid">
+            <h1><center>Webboard OAsiTz</center></h1>
+                <?php include "nav.php"; ?>
+                <br>
+                <div class="d-flex justify-content-between">
+                    <div>
+                        <label>หมวดหมู่</label>
+                            <span class="dropdown">
+                                <button class="btn btn-light dropdown-toggle btn-sm" type="button" id="button2" data-bs-toggle="dropdown" aria-expanded="false">--ทั้งหมด--</button>
+                                <ul class="dropdown-menu" aria-labelledby="button2">
+                                    <li><a href="#" class="dropdown-item">ทั้งหมด</a></li>
+                                    <li><a href="#" class="dropdown-item">เรื่องเรียน</a></li>
+                                    <li><a href="#" class="dropdown-item">เรื่องทั้วไป</a></li>
+                                </ul>
+                            </span>
+                    </div>
+                </div>
+                <br>
+                <table class="table table-striped">
+                <?php
+                    for($i=1; $i<=10; $i++){
+                        echo "<tr><td><a href=post.php?id=$i style=text-decoration:none> กระทู้ที่ $i </a></td></tr>";
+                    }
+                ?>   
+                </table>
+        </div>
+    </body>
     <?php
-        for($i = 1 ; $i<=10;$i++){
-            echo "<li><a href=post.php?id="."$i".">กระทู้ที่".$i."</a></li>";
-        }
-        ?>
-    </form>
-    </ul>
-    </div>
-</body>
-<?php
     }else{
-?>
-<body>
-    <div class="container-fluid">
-    <h1><center>Webboard OAsiTz</center></h1>
-    <?php include "nav.php";
     ?>
-    <hr>
-    หมวดหมู่:
-    <select name = "category">
-        <option value="all">--ทั้งหมด--</option>
-        <option value="general">เรื่องทั่วไป</option>
-        <option value="study">เรื่องเรียน</option>
-    </select>
-    <div style="float:right">
-        <?php
-            echo "ผู้ใช้งานในระบบ :".$_SESSION['username'];
-        ?>
-    <a href="logout.php"target="">ออกจากระบบ</a><br>
-    </div>
-    <br>
-    <a href="newpost.php">สร้างกระทู่ใหม่</a>
-    <br>
-    <ul> <form action="post.php" method="get">
+    <body>
+        <div class="container-fluid">
+            <h1><center>Webboard OAsiTz</center></h1>
+            <?php include "nav.php"; ?>
+            <br>
+                <div class="d-flex justify-content-between">
+                    <div>
+                        <label>หมวดหมู่</label>
+                            <span class="dropdown">
+                                <button class="btn btn-light dropdown-toggle btn-sm" type="button" id="button2" data-bs-toggle="dropdown" aria-expanded="false">
+                                    --ทั้งหมด--
+                                </button>
+                                <ul class="dropdown-menu" aria-labelledby="button2">
+                                    <li><a href="#" class="dropdown-item">ทั้งหมด</a></li>
+                                    <li><a href="#" class="dropdown-item">เรื่องเรียน</a></li>
+                                    <li><a href="#" class="dropdown-item">เรื่องทั้วไป</a></li>
+                                </ul>
+                            </span>
+                    </div>
+                    <div><a href="newpost.php" class="btn btn-success btn-sm"><i class="bi bi-plus"></i>สร้างกระทู้ใหม่</a></div>
+                </div>
+            <br>
+            <table class="table table-striped">
+            <form action="post.php" method="get">
+            <?php
+                for($i=1; $i<=10; $i++){
+                    echo "<tr><td><a href=post.php?id=$i style=text-decoration:none>กระทู้ที่ $i</a></td>";
+                    if($_SESSION['role']=='a'){
+                        echo "<td><a href=delete.php?id=$i class='btn btn-danger btn-sm'><i class='bi bi-trash'></i></a></td>";
+                    }
+                    echo "</tr>";
+                }
+            ?>
+            </form></table>
+        </div>
+    </body>
     <?php
-        for($i = 1 ; $i<=10;$i++){
-            echo "<li>";
-            echo "<a href=post.php?id="."$i".">กระทู้ที่".$i."</a>";
-            if($_SESSION["role"] == "a"){
-                echo "&nbsp;&nbsp;<a href=delete.php?id=$i>ลบ</a>";
-            }
-            echo "</li>";
-
-        }
-        ?>
-    </form>
-    </ul>
-    </div>
-</body>
-<?php
     }
-?>
+    ?>
 </html>
---
+++ register.php
@@ -0,0 +1,89 @@
+    <!-- CSS only -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
+    <!-- JavaScript Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
+<?php
+    session_start();
+    if(isset($_SESSION['id'])){
+    header("location:index.php");
+    die();
+    }
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div class="container">
+    <h1><center>Webboard OAsiTz</center></h1>
+    <?php include "nav.php"; ?>
+    <div class="row mt-5">
+        <div class="col-md-3"></div>
+        <div class="col-md-6">
+            <div class="card text-dark bg-white border-primary">
+                <div class="card-header bg-primary text-white">กรอกข้อมูล</div>
+                <div class="card-body">
+                    <form action="register_save.php" method="post">
+                        <div class="row mb-3">
+                            <label class="col-md-3 col-form-label">ชื่อบัญชี:</label>
+                            <div class="col-md-9">
+                                <input type="text" name="login" class="form-control" required>
+                            </div>
+                        </div>
+                        <div class="row mb-2">
+                            <label class="col-md-3 col-form-label">รหัสผ่าน:</label>
+                            <div class="col-md-9">
+                                <input type="password" name="pass" class="form-control" required>
+                            </div>
+                        </div>
+                        <div class="row mb-3">
+                            <label class="col-md-3 col-form-label">ชื่อ-นามสกุล:</label>
+                            <div class="col-md-9">
+                                <input type="text" name="name" class="form-control" required>
+                            </div>
+                        </div>
+                        <div class="row mb-3">
+                            <label class="col-md-3 col-form-label pt-0">เพศ:</label>
+                            <div class="col-md-9">
+                                <div class="form-check">
+                                    <input type="radio" name="gender" value="m" class="form-check-input"required>
+                                    <label class="form-check-label">ชาย</label>
+                                </div>
+                                <div class="form-check">
+                                    <input type="radio" name="gender" value="f" class="form-check-input"required>
+                                    <label class="form-check-label">หญิง</label>
+                                </div>
+                                <div class="form-check">
+                                    <input type="radio" name="gender" value="o" class="form-check-input"required>
+                                    <label class="form-check-label">อื่นๆ</label>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row mb-3">
+                            <label class="col-md-3 col-form-label">อีเมล:</label>
+                            <div class="col-md-9">
+                                <input type="email" name="email" class="form-control" required>
+                            </div>
+                        </div>
+                        <div class="row mb-3">
+                            <label class="col-md-3 col-form-label"></label>
+                            <div class="col-md-9">
+                                <button class="btn btn-primary btn-sm" type="submit">
+                                 <i class="bi bi-same me-1"></i>สมัครสมาชิก
+                                </button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-3"></div>
+    </div>
+    </div>
+</body>
+</html>
